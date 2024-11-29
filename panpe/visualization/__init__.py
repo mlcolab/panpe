d958fbe5bb032652b5fbca2f6d54e91560ee430f
Vladimir Starostin <vladimir.starostin@uni-tuebingen.de>
2024-11-29 15:42:28 +0100

    update license and packaging

--- panpe/visualization/__init__.py
+++ panpe/visualization/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 #
 #
-# This source code is licensed under the GPL license found in the
+# This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
